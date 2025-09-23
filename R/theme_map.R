74aec8d873eaf65e54242649b274fea62041fbd8
RichardJTelford <Richard.Telford@uib.no>
2025-09-23 17:57:58 +0200

    replace size with linewidth to stop ggplot2 warning. fixes #48

--- R/theme_map.R
+++ R/theme_map.R
@@ -11,11 +11,11 @@
 theme_map <- function(..., grid.col, grid.size) {
     theme_bw(...) %+replace%
       theme(panel.background = element_blank(),
-      panel.border = element_rect(fill = NA, colour = "black", size = 0.2),
+      panel.border = element_rect(fill = NA, colour = "black", linewidth = 0.2),
       panel.grid = element_line(colour = grid.col, linewidth = grid.size),
       plot.background = element_blank(),
-      axis.ticks.x = element_line(colour = "black", size = 0.2),
-      axis.ticks.y = element_line(colour = "black", size = 0.2),
+      axis.ticks.x = element_line(colour = "black", linewidth = 0.2),
+      axis.ticks.y = element_line(colour = "black", linewidth = 0.2),
       axis.line.x = element_blank(),
       axis.line.y = element_blank(),
       panel.ontop = TRUE
